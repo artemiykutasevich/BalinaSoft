10b1a5357028720fb8c17c6ee3949f089ee7b932
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-05-05 21:29:06 +0300

    Initial Commit

---
+++ BalinaSoft/BalinaSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalinaSoftApp.swift
+//  BalinaSoft
+//
+//  Created by Artem Kutasevich on 5.05.22.
+//
+
+import SwiftUI
+
+@main
+struct BalinaSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalinaSoft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BalinaSoft
+//
+//  Created by Artem Kutasevich on 5.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
